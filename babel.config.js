2333b4fdb181753bf845d2e0d9479a87c2926267
pratyush180403 <dubeypratyush9999@gmail.com>
2025-05-23 19:06:08 +0530

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
       'expo-router/babel',
       '@babel/plugin-proposal-export-namespace-from',
