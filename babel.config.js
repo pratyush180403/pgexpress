8e6191ed1fdb0351026e9fe383eb75f9d6f1b9ce
pratyush180403 <dubeypratyush9999@gmail.com>
2025-05-24 14:55:48 +0530

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',
-      '@babel/plugin-transform-export-namespace-from',
+      '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
     ],
   };
